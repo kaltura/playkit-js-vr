179d7454c6b21a682891433ce446d6a305be9a36
Yair Ansbacher <yair.ansbacher@kaltura.com>
2018-07-03 13:06:54 +0300

    fix(FEC-8363): VR is not working on iOS < 11.3 (#7)
    Throw a critical error for unsupported browsers:
    - Safari desktop < 11
    - iOS < 11.3.
    - ie  !== 11 on win-8.1 or win-10 
    
    depends on kaltura/playkit-js#257

---
+++ src/errors.js
@@ -0,0 +1,14 @@
+// @flow
+
+/**
+ * The VR plugin errors.
+ * @type {Object}
+ */
+const Error: {[error: string]: string} = {
+  UNSUPPORTED_BROWSER: "Your browser doesn't support features to enable VR experience",
+  PLAYSINLINE: 'playsinline must be true for VR experience',
+  DRM: 'Cannot apply VR experience for DRM content',
+  VIDEO_SIZE: 'Unable to obtain the video size for VR canvas'
+};
+
+export {Error};
