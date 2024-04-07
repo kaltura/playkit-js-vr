924e7981d9a011021b7a5e6e8e50d8c23d50a23f
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-04-07 11:05:33 +0300

    change correlated fixed text in error

--- src/errors.js
+++ src/errors.js
@@ -5,10 +5,13 @@
  * @type {Object}
  */
 const Error: {[error: string]: {message: string, id: string}} = {
-  UNSUPPORTED_BROWSER: {message: "Your browser doesn't support features to enable VR experience", id: 'vr.unsupported_browser_error_message'},
-  PLAYSINLINE: {message: 'Configuration playsinline must be true for VR experience on iPhone device.', id: 'vr.playsinline_error_message'},
-  DRM: {message: 'Cannot apply VR experience for DRM content', id: 'vr.drm_error_message'},
-  VIDEO_SIZE: {message: 'Unable to obtain the video size for VR canvas', id: 'vr.video_size_error_message'}
+  UNSUPPORTED_BROWSER: {
+    message: "Your browser doesn't support the features that are required to enable VR experience.",
+    id: 'vr.unsupported_browser_error_message'
+  },
+  PLAYSINLINE: {message: 'The playsinline configuration must be true for VR experience on iPhone device.', id: 'vr.playsinline_error_message'},
+  DRM: {message: 'Cannot apply VR experience for DRM content.', id: 'vr.drm_error_message'},
+  VIDEO_SIZE: {message: 'Unable to obtain the video size for VR canvas.', id: 'vr.video_size_error_message'}
 };
 
 export {Error};
