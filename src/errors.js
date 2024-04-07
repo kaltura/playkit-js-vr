8e60b422ead0dc392cefb93db1d9171fcbfbdf1a
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-04-07 14:54:46 +0300

    feat(FEC-13652): add meaningful error message (#106)
    ### Description of the Changes
    
    Add meaningful error message feature.
    
    - when firing a critical error from the plugin, also pass `errorDetails` as parameter which provides error title and a specific error message
    - add support for `jsx`, for using `Text` 
    - add `en.i18n.json` file for error translations
    
    <img width="790" alt="image" src="https://github.com/kaltura/playkit-js-vr/assets/79077248/1dbb5e6a-e22e-4dc1-a2dd-7e11ef0e020a">
    
    Solves FEC-13652

--- src/errors.js
+++ src/errors.js
@@ -4,11 +4,14 @@
  * The VR plugin errors.
  * @type {Object}
  */
-const Error: {[error: string]: string} = {
-  UNSUPPORTED_BROWSER: "Your browser doesn't support features to enable VR experience",
-  PLAYSINLINE: 'playsinline must be true for VR experience',
-  DRM: 'Cannot apply VR experience for DRM content',
-  VIDEO_SIZE: 'Unable to obtain the video size for VR canvas'
+const Error: {[error: string]: {message: string, id: string}} = {
+  UNSUPPORTED_BROWSER: {
+    message: "Your browser doesn't support the features that are required to enable VR experience.",
+    id: 'vr.unsupported_browser_error_message'
+  },
+  PLAYSINLINE: {message: 'The playsinline configuration must be true for VR experience on iPhone device.', id: 'vr.playsinline_error_message'},
+  DRM: {message: 'Cannot apply VR experience for DRM content.', id: 'vr.drm_error_message'},
+  VIDEO_SIZE: {message: 'Unable to obtain the video size for VR canvas.', id: 'vr.video_size_error_message'}
 };
 
 export {Error};
