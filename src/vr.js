f57bc3afb07652ea0fc46d4b0f145796b0be021f
Tzipi <101048005+Tzipi-kaltura@users.noreply.github.com>
2025-11-16 09:31:08 +0200

    fix(ADA-2863): [PSU] 360 Videos keyboard navigation (WASD) does not work with V7 player (#111)
    Description of the Changes
    Adding keyboard navigation support.

--- src/vr.js
+++ src/vr.js
@@ -69,6 +69,12 @@ class Vr extends BasePlugin {
       aspect: 640 / 360,
       near: 0.1,
       far: 1000
+    },
+    keyboardShortcutsMap: {
+      left: 65, // 'A'
+      up: 87, // 'W'
+      right: 68, // 'D'
+      down: 83 // 'S'
     }
   };
 
@@ -203,6 +209,8 @@ class Vr extends BasePlugin {
       this.eventManager.listen(window, 'mouseup', this._onDocumentPointerUp.bind(this));
       this.eventManager.listen(window, 'touchend', this._onDocumentPointerUp.bind(this));
     }
+    this.eventManager.listen(window, 'keydown', e => this._onKeyDown(e));
+
     if (window.DeviceMotionEvent) {
       this.eventManager.listen(window, 'devicemotion', this._onDeviceMotion.bind(this));
     }
@@ -483,6 +491,23 @@ class Vr extends BasePlugin {
 
   _onDocumentPointerUp(): void {
     this._pointerDown = false;
+  }
+
+  _onKeyDown(event: any): void {
+    const moveAmount = this.config.moveMultiplier * 10;
+    const shortcuts = this.config.keyboardShortcutsMap;
+    if (event.keyCode === shortcuts.left) {
+      this._longitude -= moveAmount;
+    }
+    if (event.keyCode === shortcuts.right) {
+      this._longitude += moveAmount;
+    }
+    if (event.keyCode === shortcuts.up) {
+      this._latitude += moveAmount;
+    }
+    if (event.keyCode === shortcuts.down) {
+      this._latitude -= moveAmount;
+    }
   }
 
   _getMobileVibrationValue(): number {
